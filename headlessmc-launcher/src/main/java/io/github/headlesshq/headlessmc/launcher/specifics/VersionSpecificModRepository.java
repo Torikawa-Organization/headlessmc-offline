77f7a0619c9d09cc0d86237548b2522f421a99a7
okafke <65917827+okafke@users.noreply.github.com>
2025-12-20 23:00:22 +0100

    feat(specifics): fetch latest Github Release of specifics mods (#379)
    * feat(hmc-specifics): fetch latest Github Release of specifics mods
    
    * test(specifics): Added test and cleaned up a bit
    
    * fix(specifics): add deprecated legacy method for VersionSpecificModRepository
    
    * test: Disable GitHub API test
    
    * test(integration): Fix SecurityManager

--- headlessmc-launcher/src/main/java/io/github/headlesshq/headlessmc/launcher/specifics/VersionSpecificModRepository.java
+++ headlessmc-launcher/src/main/java/io/github/headlesshq/headlessmc/launcher/specifics/VersionSpecificModRepository.java
@@ -1,8 +1,10 @@
 package io.github.headlesshq.headlessmc.launcher.specifics;
 
-import lombok.Data;
 import io.github.headlesshq.headlessmc.api.HasName;
+import io.github.headlesshq.headlessmc.launcher.download.DownloadService;
+import lombok.*;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Objects;
@@ -10,6 +12,7 @@ import java.util.regex.Pattern;
 
 /**
  * Represents a repository that provides downloads for a version specific mod, e.g. the hmc-specifics.
+ *
  * @see VersionSpecificMods
  * @see <a href=https://github.com/headlesshq/hmc-specifics>https://github.com/headlesshq/hmc-specifics</a>
  * @see <a href=https://github.com/headlesshq/mc-runtime-test>https://github.com/headlesshq/mc-runtime-test</a>
@@ -17,23 +20,32 @@ import java.util.regex.Pattern;
  */
 @Data
 public class VersionSpecificModRepository implements HasName {
+    @Getter(AccessLevel.NONE)
+    private volatile GithubReleaseService.GithubRelease latestRelease;
+    @Getter(AccessLevel.NONE)
+    private final Object lock = new Object();
+
     /**
      * The URL to download this version specific mod from.
      * This could be some a maven URL that points to the package the mod is in, or a github release url.
      * <p>E.g. <a href=https://github.com/headlesshq/hmc-specifics/releases/download/>
      * https://github.com/headlesshq/hmc-specifics/releases/download/</a>
      */
+    @EqualsAndHashCode.Exclude
     private final URL url;
+    /**
+     * The owner of this Github repository.
+     */
+    private final String owner;
     /**
      * The name of the version specific mod.
      * <p>E.g. hmc-specifics
      */
     private final String name;
     /**
-     * The release version of the mod (not the mc version).
-     * <p>E.g. 2.0.0
+     * A fallback version if GithubReleases cannot be fetched.
      */
-    private final String version;
+    private final String fallbackVersion;
     /**
      * An appendix for the jar files.
      * <p>E.g. -release
@@ -48,24 +60,24 @@ public class VersionSpecificModRepository implements HasName {
      * @param versionInfo the versionInfo containing the version and modlauncher.
      * @return the filename for the version specific mod release for the specified version and modlauncher.
      */
-    public String getFileName(VersionInfo versionInfo) {
+    public String getFileName(String version, VersionInfo versionInfo) {
         return name + "-" + versionInfo.getVersion() + "-" + version + "-"
-            + Objects.requireNonNull(versionInfo.getModlauncher(), "modlauncher was null").getHmcName()
-            + appendix + ".jar";
+                + Objects.requireNonNull(versionInfo.getModlauncher(), "modlauncher was null").getHmcName()
+                + appendix + ".jar";
     }
 
     /**
      * Returns the download URL for a release of the version specific mod.
      * This is done by concatenating the url, version number and getFileName.
      * <p>E.g. <a href=https://github.com/headlesshq/hmc-specifics/releases/download/2.0.0/hmc-specifics-1.12.2-2.0.0-fabric-release.jar>
-     *     https://github.com/headlesshq/hmc-specifics/releases/download/2.0.0/hmc-specifics-1.12.2-2.0.0-fabric-release.jar</a>
+     * https://github.com/headlesshq/hmc-specifics/releases/download/2.0.0/hmc-specifics-1.12.2-2.0.0-fabric-release.jar</a>
      *
      * @param versionInfo the versionInfo containing the version and modlauncher.
      * @return the download URL for the version specific mod release for the specified version and modlauncher.
      * @throws MalformedURLException if the resulting URL would be malformed.
      */
-    public URL getDownloadURL(VersionInfo versionInfo) throws MalformedURLException {
-        return new URL(url + version + "/" + getFileName(versionInfo));
+    public URL getDownloadURL(String version, VersionInfo versionInfo) throws MalformedURLException {
+        return new URL(url + version + "/" + getFileName(version, versionInfo));
     }
 
     /**
@@ -75,6 +87,38 @@ public class VersionSpecificModRepository implements HasName {
      */
     public Pattern getFileNamePattern() {
         return Pattern.compile(name + "-.*-.*-.*" + appendix + ".jar");
+    }
+
+    /**
+     * Legacy {@link #getVersion(DownloadService)}.
+     *
+     * @return the tag name of the latest GitHub release version of this repository.
+     * @deprecated use {@link #getVersion(DownloadService)} instead.
+     */
+    @Deprecated
+    @SneakyThrows
+    public String getVersion() {
+        return getVersion(new DownloadService());
+    }
+
+    /**
+     * Caches and downloads the current latest release for this VersionSpecificModRepository.
+     *
+     * @param downloadService the service to use to GET the latest release
+     * @return the tag_name of the latest GitHub Release of this repository
+     * @throws IOException if something goes wrong while fetching the latest GitHub Release.
+     */
+    public String getVersion(DownloadService downloadService) throws IOException {
+        if (latestRelease == null) {
+            synchronized (lock) {
+                if (latestRelease == null) {
+                    GithubReleaseService releaseService = new GithubReleaseService(downloadService);
+                    latestRelease = releaseService.getGithubRelease(owner, name);
+                }
+            }
+        }
+
+        return latestRelease == null ? fallbackVersion : latestRelease.tag_name;
     }
 
 }
--- headlessmc-launcher/src/test/java/io/github/headlesshq/headlessmc/launcher/specifics/VersionSpecificModRepositoryTest.java
+++ headlessmc-launcher/src/test/java/io/github/headlesshq/headlessmc/launcher/specifics/VersionSpecificModRepositoryTest.java
@@ -1,22 +1,23 @@
 package io.github.headlesshq.headlessmc.launcher.specifics;
 
+import io.github.headlesshq.headlessmc.launcher.download.DownloadService;
 import io.github.headlesshq.headlessmc.launcher.modlauncher.Modlauncher;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.net.MalformedURLException;
+import java.io.IOException;
 import java.net.URL;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VersionSpecificModRepositoryTest {
     @Test
-    public void testVersionSpecificModRepository() throws MalformedURLException {
-        URL url = new URL("https://github.com/headlesshq/mc-runtime-test/releases/download/");
-        VersionSpecificModRepository mcRuntimeTest = new VersionSpecificModRepository(url, "mc-runtime-test", "2.2.0", "-release");
-        assertEquals("mc-runtime-test-1.21-2.2.0-neoforge-release.jar", mcRuntimeTest.getFileName(new VersionInfo("1.21", Modlauncher.NEOFORGE)));
-        assertEquals("mc-runtime-test-1.19.2-2.2.0-fabric-release.jar", mcRuntimeTest.getFileName(new VersionInfo("1.19.2", Modlauncher.FABRIC)));
-        URL expected = new URL("https://github.com/headlesshq/mc-runtime-test/releases/download/2.2.0/mc-runtime-test-1.12.2-2.2.0-lexforge-release.jar");
-        assertEquals(expected, mcRuntimeTest.getDownloadURL(new VersionInfo("1.12.2", Modlauncher.LEXFORGE)));
+    @Disabled("Requests the GitHub API")
+    public void testVersionSpecificModRepository() throws IOException {
+        URL url = new URL("https://github.com/headlesshq/hmc-test-repo/releases/download/");
+        VersionSpecificModRepository hmcTestRepo = new VersionSpecificModRepository(url, "headlesshq", "hmc-test-repo", "0.2.0", "-release");
+        assertEquals("0.1.0", hmcTestRepo.getVersion(new DownloadService()));
+        assertEquals("hmc-test-repo-1.21.11-0.1.0-lexforge-release.jar", hmcTestRepo.getFileName(hmcTestRepo.getVersion(new DownloadService()), new VersionInfo("1.21.11", Modlauncher.LEXFORGE)));
     }
 
 }
